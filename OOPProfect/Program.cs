d312187c83a1d4813370ca06d6ed694a96153316
YanaRyazanova <yanaryazanovs@gmaul.com>
2020-10-20 11:17:09 +0500

    Add project files.

---
+++ OOPProfect/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOPProfect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
