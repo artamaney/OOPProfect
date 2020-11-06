bd42b6c25e014674c68602929fa5407def6404e8
YanaRyazanova <yanaryazanovs@gmaul.com>
2020-11-06 20:08:58 +0500

    add .csproj assembly, Domain.LessonReminder, Tests

---
+++ Bot/Domain/Functions/LessonReminder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bot.Domain.Functions
+{
+    public class LessonReminder: IBotFunction
+    {
+        static public string Do(DateTime lessonStartTime)
+        {
+            while (true)
+            {
+                if (DateTime.Now.AddMinutes(10) == lessonStartTime)
+                {
+                    return "Пара сейчас начнется";
+                }
+            }
+        }
+    }
+}
---
+++ Tests/DomainTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Bot.Domain.Functions;
+
+namespace Tests
+{
+    [TestFixture]
+    public class DomainTest
+    {
+        [Test]
+        public void LessonReminder()
+        {
+            Assert.AreEqual("Пара сейчас начнется",
+                Bot.Domain.Functions.LessonReminder.Do(DateTime.Now.AddMinutes(11)));
+        }
+    }
+}
